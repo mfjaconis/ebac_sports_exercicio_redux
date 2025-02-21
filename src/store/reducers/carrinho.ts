02782c563115bcfb1200c8b2d1a0ff6597f37e70
mfjaconis <matheusjaconis@gmail.com>
2025-02-21 16:41:09 -0300

    criando reducer action de carrinho

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,28 @@
+import { type PayloadAction, createSlice } from "@reduxjs/toolkit";
+import type { Produto } from "../../App";
+
+type CarrinhoState = {
+  itens: Produto[]
+}
+
+const initialState: CarrinhoState = {
+  itens: []
+}
+
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers:{
+    adicionar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload;
+
+      if(state.itens.find((p) => p.id === produto.id)){
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const {adicionar } = carrinhoSlice.actions;
